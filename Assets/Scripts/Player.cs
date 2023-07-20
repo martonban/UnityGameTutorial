a4edaa6d2768e015d5a7b84766eaae4fc2f1d55b
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-20 11:30:39 +0200

    Character Add + Rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,16 +12,16 @@ public class Player : MonoBehaviour
         Vector2 inputVector = new Vector2(0,0);
 
         if(Input.GetKey(KeyCode.W)) {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
             inputVector.y = -1;
         }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y = 1;
+        }
         if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
+            inputVector.x = 1;
         }
         if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = 1;
+            inputVector.x = -1;
         }
 
         inputVector = inputVector.normalized;
@@ -30,7 +30,8 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        Debug.Log(inputVector);
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
 }
