9081ced93e74101cadd2c505c4a495673eedf047
Márton Bán <30813844+martonban@users.noreply.github.com>
2023-07-21 11:34:55 +0200

    Object Interaction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,41 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
 
     private void Update() {
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    public bool IsWalking() { 
+        return isWalking;
+    }
+
+
+    private void HandleInteraction() {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+
+    }
+
+    private void HandleMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
@@ -39,10 +70,10 @@ public class Player : MonoBehaviour
                 if (canMove) {
                     //Can move only on the Z
                     moveDir = moveDirZ;
-                } else { 
+                } else {
                     // Cannot move in any direction
                 }
-            
+
             }
         }
 
@@ -54,10 +85,6 @@ public class Player : MonoBehaviour
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking() { 
-        return isWalking;
     }
 
 }
